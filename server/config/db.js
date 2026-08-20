7ed526d5e649dbff95c459e53fc8cb5d8ec47c7c
Praseeth2001 <praseeth17@gmail.com>
2026-08-20 22:15:06 +0530

    Added data to collection

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, { dbName: "holiday-booking" });
+    const conn = await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DBNAME });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`MongoDB connection error: ${err.message}`);
